745f5105c9e4f360554342790d7fa7e4aa27e793
Umar Lateef <umar.sd@hotmail.com>
2022-07-02 10:16:46 -0500

    Update PhoneCombination.js

--- BackTracking/PhoneCombination.js
+++ BackTracking/PhoneCombination.js
@@ -1,5 +1,6 @@
-let digits = [2,5];
-let letters = {
+var letterCombinations = function(digits) {
+    if(!digits) return [];
+    let letters = {
         '2':['A','B','C'],
         '3':['D','E','F'],
         '4':['G','H','I'],
@@ -9,35 +10,18 @@ let letters = {
         '8':['T','U','V'],
         '9':['W','X','Y','Z']
     }
-    let arrays = [];
-    let visited = [];
-    for(let num of digits){
-        
+    let arrays = [];    
+    for(let num of digits){        
         arrays.push(letters[num]);
-        visited.push(new Array(letters[num].length).fill(false))
-        //arrays[num]= letters[num]
     }
     function dfs(path, index, resp){
-        if(path[index] && path[index].length === digits.length) {
-            index++;
-            return resp.push(path);
+        if(path.length === digits.length) return resp.push(path.toLowerCase());
+        //console.log(arrays);        
+        for(let letter of arrays[index]){
+            dfs(path+letter, index+1, resp);
         }
-        console.log(index, visited[index].length)
-        //return
-        if(visited[index]){
-            path[index] = '';
-            for(let inIndex = 0; inIndex< digits.length; inIndex++){
-                if(visited[inIndex][index]) continue;
-                path[index] += arrays[inIndex][index];
-                visited[inIndex][index]=true;
-            
-            }
-            dfs(path, index, resp);
-        }
-        //for(
     }
-    // WRITE YOUR BRILLIANT CODE HERE
-    
     let resp = [];
-    dfs([], 0, resp);
-    console.log(resp);
+    dfs('', 0, resp);
+    return resp;
+};
